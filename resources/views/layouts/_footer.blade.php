9edcbb79bf8ddf7d55db25d0282565d7516012ac
Xtomatoziyang <tomatoziyang@gmail.com>
2016-12-15 11:03:21 +0800

    update

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,13 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small>
+            Made with love by <a href="https://github.com/summerblue">Summer</a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="/about">关于</a></li>
+                <li><a href="/help">帮助</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
